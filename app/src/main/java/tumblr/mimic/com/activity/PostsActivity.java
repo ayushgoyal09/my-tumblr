b35ee636cd74ee583337f9dac2cc3bfca1d57959
Ayush Goyal <ayushgoyal09@gmail.com>
2016-09-21 15:41:00 -0700

    initial commit.

---
+++ app/src/main/java/tumblr/mimic/com/activity/PostsActivity.java
@@ -0,0 +1,15 @@
+package tumblr.mimic.com.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tumblr.mimic.com.myapplication.R;
+
+public class PostsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts);
+    }
+}
