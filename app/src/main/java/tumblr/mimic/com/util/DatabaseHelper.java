6a833590a27bd3653069820a5ea0fd4f9d40a898
Ayush Goyal <ayushgoyal09@gmail.com>
2016-09-23 14:28:55 -0700

    add database helper with insert and primary key lookup methods

---
+++ app/src/main/java/tumblr/mimic/com/util/DatabaseHelper.java
@@ -0,0 +1,56 @@
+package tumblr.mimic.com.util;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Ayush on 9/22/2016.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "posts.db";
+    public static final String TABLE_NAME = "posts_table";
+    public static final String COL_1 = "id";
+    public static final String COL_2 = "location";
+    public static final String COL_3 = "caption";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table "+TABLE_NAME+" (ID INTEGER PRIMARY KEY,LOCATION TEXT, CAPTION TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(long id, String location, String caption){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,location);
+        contentValues.put(COL_3,caption);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if (result == -1){
+            return false;
+        }else
+            return true;
+    }
+
+    public Cursor checkPrimaryKey(long id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME+" where id = "+ id,null);
+        return res;
+    }
+
+
+}
