341d9f8a94d47a80f9df5b3dc94bc31fded3ff9c
Ayush Goyal <ayushgoyal09@gmail.com>
2016-09-23 19:38:46 -0700

    save image file to disk

--- app/src/main/java/tumblr/mimic/com/adapter/PostsDataAdapter.java
+++ app/src/main/java/tumblr/mimic/com/adapter/PostsDataAdapter.java
@@ -1,17 +1,22 @@
 package tumblr.mimic.com.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Environment;
 import android.support.v7.widget.RecyclerView;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import tumblr.mimic.com.bean.PostBean;
@@ -29,6 +34,9 @@ public class PostsDataAdapter extends RecyclerView.Adapter<PostsDataAdapter.View
     public PostsDataAdapter(Context context,ArrayList<PostBean> posts) {
         this.posts = posts;
         this.context = context;
+        for(PostBean post : posts){
+            Log.i("id --",post.getId()+"");
+        }
     }
 
     @Override
@@ -38,11 +46,43 @@ public class PostsDataAdapter extends RecyclerView.Adapter<PostsDataAdapter.View
     }
 
     @Override
-    public void onBindViewHolder(PostsDataAdapter.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(PostsDataAdapter.ViewHolder viewHolder, final int position) {
+
+        Target target = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath()  + "/"+ posts.get(position).getId() + ".jpg");
+                try {
+                    file.createNewFile();
+                    FileOutputStream ostream = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG,100,ostream);
+                    ostream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+
+
         viewHolder.postTitle.setText(posts.get(position).getPostTitle());
-        Picasso.with(context).load(posts.get(position).getPostImage()).into(viewHolder.postImage);
+        Log.i("POSTS IMAGE",posts.get(position).getPostImage().toString());
+        Picasso.with(context).load(posts.get(position).getPostImage()).into(target);
         viewHolder.postTitle.setMovementMethod(LinkMovementMethod.getInstance());
 //        Glide.with(context).load(posts.get(position)).asGif().into(viewHolder.postImage);
+
+
+
     }
 
     @Override
